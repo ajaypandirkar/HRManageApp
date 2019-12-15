cc9e87249296fd634e9bf64258f9edaa147ff45b
ajaypandirkar <53173843+ajaypandirkar@users.noreply.github.com>
2019-12-15 16:26:42 +0530

    Initial Commit

---
+++ HRManage.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using HRManage.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRManage.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
