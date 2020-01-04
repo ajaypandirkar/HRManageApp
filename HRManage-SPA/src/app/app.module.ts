19ff77f35eaab1a25b73c59306bf60b73527d14d
ajaypandirkar <53173843+ajaypandirkar@users.noreply.github.com>
2020-01-04 14:41:12 +0530

    Routes Added

--- HRManage-SPA/src/app/app.module.ts
+++ HRManage-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -10,6 +11,10 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptor } from './_services/error.interceptor';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
 
 
 
@@ -18,21 +23,20 @@ import { ErrorInterceptor } from './_services/error.interceptor';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      EmployeeListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
-      {
-         provide: HTTP_INTERCEPTORS,
-         useClass: ErrorInterceptor,
-         multi: true
-       }
    ],
    bootstrap: [
       AppComponent
